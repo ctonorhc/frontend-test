df3f5a38f7b0e753cc0bc68517a14b62361317a7
Silla Tan <silla.tan@gmail.com>
2016-03-19 17:14:10 +1100

    style list

--- src/components/counter/list/entry.jsx
+++ src/components/counter/list/entry.jsx
@@ -3,28 +3,36 @@ import Reactor from 'reactor';
 
 import CounterModule from 'modules/counter';
 
+import Clear from 'material-ui/lib/svg-icons/content/Clear';
+import Add from 'material-ui/lib/svg-icons/content/Add';
+import Remove from 'material-ui/lib/svg-icons/content/Remove';
+
+import styles from './styles';
+
 export default React.createClass({
 
   render() {
     const { counter } = this.props;
 
     return (
-      <div>
-        <button onClick={ () => CounterModule.actions.removeCounter(counter.get('id')) }>
-          X
-        </button>
-        <span>
-          { counter.get('title') }
-        </span>
-        <button onClick={ () => CounterModule.actions.decrementCounter(counter.get('id')) }>
-          -
-        </button>
-        <span>
-          { counter.get('count') }
-        </span>
-        <button onClick={ () => CounterModule.actions.incrementCounter(counter.get('id')) }>
-          +
-        </button>
+      <div style={ styles.entry }>
+        <Clear style={ styles.clear}
+               onClick={ () => CounterModule.actions.removeCounter(counter.get('id')) } />
+        <div style={ styles.label }>
+          <div>
+            { counter.get('title') }
+          </div>
+          <div style={ styles.count }>
+            { counter.get('count') }
+          </div>
+        </div>
+        <div style={ styles.modifiers }>
+          <Remove style={ styles.remove }
+                  onClick={ () => CounterModule.actions.decrementCounter(counter.get('id')) } />
+          <Add style={ styles.add }
+               onClick={ () => CounterModule.actions.incrementCounter(counter.get('id')) } />
+        </div>
+
       </div>
     );
   }
---
+++ src/components/counter/list/styles.js
@@ -0,0 +1,43 @@
+export default {
+  entry: {
+    display: 'flex',
+    alignItems: 'center',
+    flex: '0 0 auto',
+    color: '#4A5485',
+    padding: 15,
+    border: `1px solid #4A5485`,
+    borderRadius: 5
+  },
+
+  label: {
+    marginLeft: 20,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+
+  modifiers: {
+    marginLeft: 'auto'
+  },
+
+  count: {
+    fontSize: 12,
+    fontColor: '#94A0E2'
+  },
+
+  clear: {
+    fill: '#B2BCF4'
+  },
+
+  add: {
+    border: `1px solid #6CC7A7`,
+    borderRadius: 5,
+    fill: '#6CC7A7'
+  },
+
+  remove: {
+    border: `1px solid #FFB18B`,
+    borderRadius: 5,
+    fill: '#FFB18B',
+    marginRight: 15
+  }
+};
