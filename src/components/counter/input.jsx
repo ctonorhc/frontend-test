06a16008c88b06d2af90d9c7335569f24b0c4608
Silla Tan <silla.tan@gmail.com>
2016-03-19 15:51:42 +1100

    partially style input

--- src/components/counter/input.jsx
+++ src/components/counter/input.jsx
@@ -4,6 +4,8 @@ import Reactor from 'reactor';
 import CounterModule from 'modules/counter';
 import Input from './input';
 import Total from './total';
+import { TextField, FlatButton } from 'material-ui';
+import LibraryAdd from 'material-ui/lib/svg-icons/av/library-add';
 
 export default React.createClass({
 
@@ -14,23 +16,23 @@ export default React.createClass({
   },
 
   createCounter() {
-    if (this.input.checkValidity()) {
-      CounterModule.actions.createCounter(this.state.value);
-    }
+    CounterModule.actions.createCounter(this.state.value);
   },
 
   render() {
     const { value } = this.state;
     return (
       <div>
-        <input value={ value }
-               onChange={ e => this.setState({ value: e.target.value }) }
-               required
-               ref={ node => this.input = node }/>
-             
-        <button onClick={ this.createCounter }>
-          +
-        </button>
+
+        <TextField value={ value }
+                   name='create-counter'
+                   hintText='Enter a name'
+                   onChange={ e => this.setState({ value: e.target.value }) } />
+        <FlatButton onClick={ this.createCounter }
+                    primary={ true }
+                    label='Create Counter'
+                    labelPosition='before'
+                    icon={ <LibraryAdd/> }/>
       </div>
     );
   }
--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,9 @@
 import { render } from 'react-dom';
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Counter from 'components/counter';
 
 render(<Counter />, document.getElementById('app'));
+
+injectTapEventPlugin();
